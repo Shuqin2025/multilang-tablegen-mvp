28149145db4fb9b10f5903f685f33546c76e2b89
Shuqin2025 <shuqinamberg@gmail.com>
2025-08-13 17:28:12 +0200

    Update main.jsx

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './pages/MultiLangTableMVP.jsx'
+import App from './MultiLangTableMVP.jsx' // 修正路径
 
 createRoot(document.getElementById('root')).render(<App />)
