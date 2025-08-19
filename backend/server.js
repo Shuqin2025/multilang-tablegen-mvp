36be5eddfb82a1f221d6ba19e007fc9bc20bc593
Shuqin2025 <shuqinamberg@gmail.com>
2025-08-19 23:47:59 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,13 @@ import { fileURLToPath } from 'url'
 import tablegenRouter from './routes/tablegen.js'
 
 dotenv.config()
+
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const app = express()
 app.use(cors())
-app.use(express.json({limit:'2mb'}))
+app.use(express.json({ limit: '2mb' }))
 
 // serve generated files
 app.use('/files', express.static(path.join(__dirname, 'files')))
